fc76d5c8eab4b9171a32a031d06846d154feb9d4
Navi03 <73278819+Navi03-cyber@users.noreply.github.com>
2023-07-26 11:36:02 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,84 @@
+create web server
+const express = require('express');
+const app = express();
+// create route
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+// start server
+app.listen(3000, function() {
+    console.log('Server is listening on port 3000. Ready to accept requests!');
+});
+
+// Path: comments.js
+// Compare this snippet from member.js:
+// function skillsMember() {
+//     console.log('I have skills')
+// }
+// create web server
+const express = require('express');
+const app = express();
+// create route
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+// start server
+app.listen(3000, function() {
+    console.log('Server is listening on port 3000. Ready to accept requests!');
+});
+
+// Path: comments.js
+// Compare this snippet from member.js:
+// function skillsMember() {
+//     console.log('I have skills')
+// }
+// create web server
+const express = require('express');
+const app = express();
+// create route
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+// start server
+app.listen(3000, function() {
+    console.log('Server is listening on port 3000. Ready to accept requests!');
+});
+
+// Path: comments.js
+// Compare this snippet from member.js:
+// function skillsMember() {
+//     console.log('I have skills')
+// }
+// create web server
+const express = require('express');
+const app = express();
+// create route
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+// start server
+app.listen(3000, function() {
+    console.log('Server is listening on port 3000. Ready to accept requests!');
+});
+
+// Path: comments.js
+// Compare this snippet from member.js:
+// function skillsMember() {
+//     console.log('I have skills')
+// }
+// create web server
+const express = require('express');
+const app = express();
+// create route
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+// start server
+app.listen(3000, function() {
+    console.log('Server is listening on port 3000. Ready to accept requests!');
+});
+
+// Path: comments.js
+// Compare this snippet from member.js:
+// function skillsMember() {
+//     console.log('I have skills')
